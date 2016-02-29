8223b0e511f22b5e09b9125f001b6ac2e350edaf
ArdiMaster <ardimaster@outlook.de>
2016-02-29 22:52:09 +0100

    Initial preparations

--- win/LK-Renderer/MainForm.Designer.cs
+++ win/LK-Renderer/MainForm.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.blenderVersionSelector = new System.Windows.Forms.ComboBox();
+            this.labelBlndrVrsnSlctr = new System.Windows.Forms.Label();
+            this.loadBlenderButton = new System.Windows.Forms.Button();
+            this.unzipper = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // blenderVersionSelector
+            // 
+            this.blenderVersionSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.blenderVersionSelector.FormattingEnabled = true;
+            this.blenderVersionSelector.Items.AddRange(new object[] {
+            "v2.76b",
+            "v2.76a",
+            "v2.76",
+            "v2.75a",
+            "v2.75"});
+            this.blenderVersionSelector.Location = new System.Drawing.Point(12, 29);
+            this.blenderVersionSelector.Name = "blenderVersionSelector";
+            this.blenderVersionSelector.Size = new System.Drawing.Size(121, 21);
+            this.blenderVersionSelector.TabIndex = 0;
+            this.blenderVersionSelector.SelectedIndexChanged += new System.EventHandler(this.blenderVersionSelector_SelectedIndexChanged);
+            // 
+            // labelBlndrVrsnSlctr
+            // 
+            this.labelBlndrVrsnSlctr.AutoSize = true;
+            this.labelBlndrVrsnSlctr.Location = new System.Drawing.Point(12, 9);
+            this.labelBlndrVrsnSlctr.Name = "labelBlndrVrsnSlctr";
+            this.labelBlndrVrsnSlctr.Size = new System.Drawing.Size(84, 13);
+            this.labelBlndrVrsnSlctr.TabIndex = 1;
+            this.labelBlndrVrsnSlctr.Text = "Blender Version:";
+            // 
+            // loadBlenderButton
+            // 
+            this.loadBlenderButton.Location = new System.Drawing.Point(12, 57);
+            this.loadBlenderButton.Name = "loadBlenderButton";
+            this.loadBlenderButton.Size = new System.Drawing.Size(121, 23);
+            this.loadBlenderButton.TabIndex = 2;
+            this.loadBlenderButton.Text = "Download";
+            this.loadBlenderButton.UseVisualStyleBackColor = true;
+            this.loadBlenderButton.Click += new System.EventHandler(this.loadBlenderButton_Click);
+            // 
+            // unzipper
+            // 
+            this.unzipper.DoWork += new System.ComponentModel.DoWorkEventHandler(this.unzipper_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(729, 547);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(189, 16);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(9, 547);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(41, 13);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "Ready.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(916, 563);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.loadBlenderButton);
+            this.Controls.Add(this.labelBlndrVrsnSlctr);
+            this.Controls.Add(this.blenderVersionSelector);
+            this.Name = "MainForm";
+            this.Text = "LK-Renderer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox blenderVersionSelector;
+        private System.Windows.Forms.Label labelBlndrVrsnSlctr;
+        private System.Windows.Forms.Button loadBlenderButton;
+        private System.ComponentModel.BackgroundWorker unzipper;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- win/LK-Renderer/MainForm.cs
+++ win/LK-Renderer/MainForm.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using Microsoft.VisualBasic.Devices;
 
 namespace LK_Renderer
 {
     public partial class MainForm : Form
     {
+        String dataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\LK-Studios\\LK-Renderer";
+        String blenderVersion = "v2.76b";
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            blenderVersionSelector.SelectedItem = "v2.76b";
+            if (!System.IO.Directory.Exists(dataFolder))
+            {
+                System.IO.Directory.CreateDirectory(dataFolder + "\\BlenderVersions");
+            }
+        }
+
+        private void loadBlenderButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void blenderVersionSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            blenderVersion = blenderVersionSelector.SelectedItem.ToString();
+            doAvailability(checkAvailability(blenderVersion));
+        }
+
+        private int checkAvailability(String version)
+        {
+            return 0;
+        }
+
+        private void doAvailability(int availability)
+        {
+            switch (availability)
+            {
+                case 0:
+                    loadBlenderButton.Text = "Download";
+                    loadBlenderButton.Enabled = true;
+                    break;
+                case 1:
+                    loadBlenderButton.Text = "Unzip";
+                    loadBlenderButton.Enabled = true;
+                    break;
+                case 2:
+                    loadBlenderButton.Text = "Available";
+                    loadBlenderButton.Enabled = false;
+                    break;
+            }
+        }
+
+        private void unzipper_DoWork(object sender, DoWorkEventArgs e)
+        {
+
         }
     }
 }
